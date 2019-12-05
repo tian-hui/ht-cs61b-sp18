f1f90542b5cf668145c80ec1e874fcb8480e9161
Tian <ht1049890979@google.com>
2019-12-06 01:52:41 +0800

    finish leapyear

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -1,7 +1,17 @@
 /** Class that determines whether or not a year is a leap year.
- *  @author YOUR NAME HERE
+ *  @author Tian Hui
  */
 public class LeapYear {
+
+    public static boolean isLeapYear(int year) {
+        if (year % 400 == 0) {
+            return true;
+        } else if (year % 4 == 0 && year % 100 != 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 
     /** Calls isLeapYear to print correct statement.
      *  @param  year to be analyzed
